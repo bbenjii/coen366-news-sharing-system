a682324da3e3b99443ef6265ef84f114a38afdf3
Ben Ollomo <benji.ollomo@gmail.com>
2026-04-02 23:27:10 -0400

    Handle connection errors and add error logging in TCP client `send_message` method

--- src/client/tcp_client.py
+++ src/client/tcp_client.py
@@ -5,7 +5,15 @@ class TcpClient:
     def send_message(self, server_config, message):
         server_address = (server_config["bind_host"], server_config["tcp_port"])
 
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as tcp_socket:
-            tcp_socket.connect(server_address)
-            print("[client] Connected to server.")
-            tcp_socket.sendall(message.encode())
+        try:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as tcp_socket:
+                tcp_socket.connect(server_address)
+                print("[client] Connected to server.")
+                tcp_socket.sendall(message.encode())
+                return True
+        except ConnectionRefusedError:
+            print(f"[client] Could not connect to {server_config['name']} at {server_address}")
+        except OSError as exc:
+            print(f"[client] TCP send failed: {exc}")
+
+        return False
